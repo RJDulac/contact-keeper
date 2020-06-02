c2204b6cf2ee6514dfe7fbaecfe56ca18ba612ae
machine <analyticrye@gmail.com>
2020-06-02 19:24:56 -0400

    added contactForm and state

---
+++ client/src/context/contact/ContactState.js
@@ -0,0 +1,83 @@
+import React, { useReducer } from "react";
+import { v4 as uuidv4 } from "uuid";
+import ContactContext from "./contactContext";
+import contactReducer from "./contactReducer";
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  SET_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACTS,
+  CLEAR_CURRENT,
+  CLEAR_FILTER,
+} from "../types";
+
+const ContactState = (props) => {
+  const intialState = {
+    contacts: [
+      {
+        id: 1,
+        name: "Jill Johnson",
+        email: "jill@gmail.com",
+        phone: "111-111-1111",
+        type: "personal",
+      },
+      {
+        id: 2,
+        name: "Ryan Dulac",
+        email: "dulac@gmail.com",
+        phone: "757-666-2345",
+        type: "personal",
+      },
+      {
+        id: 3,
+        name: "Ben Johnson",
+        email: "ben@gmail.com",
+        phone: "111-111-1111",
+        type: "professional",
+      },
+    ],
+    current: null,
+  };
+  const [state, dispatch] = useReducer(contactReducer, intialState);
+
+  //add contact
+  const addContact = (contact) => {
+    contact.id = uuidv4();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
+
+  //delete contact
+  const deleteContact = (id) => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
+  //set current contact
+  const setCurrent = (contact) => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
+  //clear current contact
+  const clearCurrent = () => {
+    dispatch({ type: CLEAR_CURRENT });
+  };
+  // update contact
+
+  //fitler contacts
+
+  //clear fitler
+  return (
+    <ContactContext.Provider
+      value={{
+        contacts: state.contacts,
+        current: state.current,
+        addContact,
+        deleteContact,
+        setCurrent,
+        clearCurrent,
+      }}
+    >
+      {props.children}
+    </ContactContext.Provider>
+  );
+};
+
+export default ContactState;
